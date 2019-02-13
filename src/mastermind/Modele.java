54d8ee5806cec2b2cf3080784bf904ccfa6d7084
Antoine Amalric <aamalr01@debian.univ-jfc.fr>
2019-02-13 11:00:31 +0100

    Classe Modèle initialisée

--- src/mastermind/Modele.java
+++ src/mastermind/Modele.java
@@ -3,10 +3,10 @@ package mastermind;
 import java.awt.Color;
 
 public class Modele {
-	Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
-	int nb_tenta;
-	int diff;
-	enum Etat { EN_COURS, GAGNE, PERDU };
+	public static Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
+	public static int nb_tenta;
+	public static int diff;
+	public static enum Etat { EN_COURS, GAGNE, PERDU };
 	
 	public Modele() {
 		this.diff = 4;
