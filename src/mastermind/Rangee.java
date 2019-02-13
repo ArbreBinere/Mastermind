9a9e164d6144c12cdfa6d0987e72628cc7045271
Nathan Tarrieu <ntarrieu@debian.univ-jfc.fr>
2019-02-13 11:23:59 +0100

    gros lapin

--- src/mastermind/Rangee.java
+++ src/mastermind/Rangee.java
@@ -1,10 +1,20 @@
 package mastermind;
 
+import java.awt.Color;
+
 public class Rangee {
 
 	int taille = Modele.diff;
+	Color[] jeton;
 	int indiceJeton;
 	int noirs;
 	int blancs;
 	int[] resultat = {noirs,blancs};
+	
+	public Rangee(){
+		jeton = new Color[taille];
+		indiceJeton = 0;
+		noirs=0;
+		blancs=0;
+	}
 }
